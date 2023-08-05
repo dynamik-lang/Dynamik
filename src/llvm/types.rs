b36e483f69e4a96202aff2a9d4477072ba6fafdb
wizard <pranjalpatel9838@gmail.com>
2023-08-05 19:03:29 +0530

    refactor

--- src/llvm/types.rs
+++ src/llvm/types.rs
@@ -1,7 +1,7 @@
 use inkwell::{
     basic_block::BasicBlock,
     types::BasicTypeEnum,
-    values::{BasicValueEnum, FloatValue, FunctionValue, IntValue, PointerValue},
+    values::{BasicValue, BasicValueEnum, FloatValue, FunctionValue, IntValue, PointerValue},
 };
 
 /// This is the most important
@@ -45,31 +45,35 @@ impl<'a> Value<'a> {
     }
     pub(crate) fn as_bool(&self) -> IntValue<'a> {
         match self {
-            Value::Bool(b) => b.clone(),
+            Value::Bool(b) => *b,
             _ => unreachable!(),
         }
     }
 
     pub(crate) fn as_basic_value(&self) -> BasicValueEnum<'a> {
         match self {
-            Value::Int(i) => i.clone().into(),
-            Value::Float(f) => f.clone().into(),
-            Value::Pointer(s) => s.clone().into(),
-            Value::Bool(b) => b.clone().into(),
+            Value::Int(i) => (*i).into(),
+            Value::Float(f) => (*f).into(),
+            Value::Pointer(s) => (*s).into(),
+            Value::Bool(b) => (*b).into(),
         }
     }
 
     pub(crate) fn is_float(&self) -> bool {
-        match self {
-            Value::Float(_) => true,
-            _ => false,
-        }
+        matches!(self, Value::Bool(_))
     }
 }
 
-impl<'ctx> Into<BasicValueEnum<'ctx>> for Value<'ctx> {
-    fn into(self) -> BasicValueEnum<'ctx> {
-        self.as_basic_value()
+impl<'ctx> From<BasicValueEnum<'ctx>> for Value<'ctx> {
+    fn from(value: BasicValueEnum<'ctx>) -> Self {
+        match value {
+            BasicValueEnum::IntValue(i) if i.get_type().get_bit_width() == 1 => Self::Bool(i),
+            BasicValueEnum::IntValue(i) => Self::Int(i),
+            BasicValueEnum::FloatValue(i) => Self::Float(i),
+            BasicValueEnum::PointerValue(i) => Self::Pointer(i),
+
+            _ => unimplemented!("Unimplemented type"),
+        }
     }
 }
 
